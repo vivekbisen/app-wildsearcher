0b95093a8afa9ba1048f36f903dbb480a4a4eb56
Vivek Bisen <vivek.bisen@sportngin.com>
2019-02-28 22:13:23 -0600

    root to cars

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cars#index'
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
