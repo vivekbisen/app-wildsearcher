2145caa98faca3ef5024801007fd8c9dcf792c34
Vivek Bisen <vivek.bisen@sportngin.com>
2019-02-28 22:36:16 -0600

    allow descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_032553) do
+ActiveRecord::Schema.define(version: 2019_03_01_043503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_032553) do
     t.string "vin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
